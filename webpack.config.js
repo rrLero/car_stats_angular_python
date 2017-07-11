73010440ba9ae5e2f7a6205fd0c3ca38582ca507
rrlero <roman@xlan.net.ua>
2017-07-11 16:53:22 +0300

    'web pack'

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by rrlero on 10.07.17.
+ */
+module.exports = {
+    entry: {
+        libs: './libs.js',
+        public: './app.module.js'
+    },
+    output: {
+        path: "/home/rrlero/Рабочий стол/coursera/car_stats_angular_python",
+        filename: "[name].bundle.js"
+
+    },
+    watch: true,
+
+};
